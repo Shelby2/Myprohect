854c235106a348ba652347bfd5f3c0df20643be7
Shelby2 <dimkakova@gmail.com>
2019-06-07 19:31:38 +0300

    Add array_unique

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
             border-radius: 25px;
             text-align: center;
         }
+
         div {
             margin: 60px;
             text-align: center;
@@ -33,44 +34,46 @@
 <body>
 <h1>First calculator on PHP</h1>
 <div>
-<?php
-$a = 2;
-$b = 5;
+    <?php
+    $a = 2;
+    $b = 5;
 
 
+    $sum = $a + $b;
+    $diff = $a - $b;
+    $multipl = $a * $b;
+    $divis = $a / $b;
+    $rem = $a % $b;
 
-$sum = $a + $b;
-$diff = $a - $b;
-$multipl = $a * $b;
-$divis = $a / $b;
-$rem = $a % $b;
-
-echo "<pre>";
-echo "Сложение a и b: ", $sum , "<br>";
-echo "Вычитание a и b: ", $diff , "<br>";
-echo "Умножение a и b: ", $multipl , "<br>";
-echo "Деление a и b: ", $divis , "<br>";
-echo "Остаток от деления a и b: ", $rem , "<br>";
-echo "Модуль числа: ", "<br>" , abs($sum) , "<br>" , abs($diff) ,"<br>" , abs($multipl) , "<br>" , abs($divis);
-echo "<br>", "Преобразование числа в восьмеричное представление в виде строки: ", "<br>" , decoct($sum) , "<br>" , decoct($diff) ,"<br>" , decoct($multipl) , "<br>" , decoct($divis);
+    echo "<pre>";
+    echo "Сложение a и b: ", $sum, "<br>";
+    echo "Вычитание a и b: ", $diff, "<br>";
+    echo "Умножение a и b: ", $multipl, "<br>";
+    echo "Деление a и b: ", $divis, "<br>";
+    echo "Остаток от деления a и b: ", $rem, "<br>";
+    echo "Модуль числа: ", "<br>", abs($sum), "<br>", abs($diff), "<br>", abs($multipl), "<br>", abs($divis);
+    echo "<br>", "Преобразование числа в восьмеричное представление в виде строки: ", "<br>", decoct($sum), "<br>", decoct($diff), "<br>", decoct($multipl), "<br>", decoct($divis);
 
 
-?>
+    ?>
 </div>
 <div>
     <?php
-        $array1 = [4,3,4,3,6,4,7,4,3,2];
-        $array = [
+    $array1 = [4, 3, 4, 3, 6, 4, 7, 4, 3, 2];
+    $array = [
         "Name" => "Dmitriy",
         "Age" => 22,
         "Login" => true,
-        ];
-        print_r(array_filter($array1));
+    ];
+    print_r(array_filter($array1));
     echo "<br>";
     echo "<pre>";
-        var_dump(count($array1));
+    var_dump(count($array1));
     echo "<br>";
-        print_r(array_chunk($array, 2));
+    print_r(array_chunk($array, 2));
+    $result = array_unique($array1);
+    echo "Только неповторяющиеся значения массива: ";
+    print_r($result);
 
     ?>
 </div>
